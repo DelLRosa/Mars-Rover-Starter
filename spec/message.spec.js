6cdddeac0cec5dc7d18b347804ad1f24b3567e8d
Delaney Green <delaney.rosa1@gmail.com>
2024-02-21 17:43:15 -0600

    complete message class + test suite

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,20 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+    it("throws error if a name is NOT passed into the contstructor as the first parameter", function(){
+        expect( function() { new Message();}).toThrow(new Error('name required.'));
+    });
+
+    it("constructor sets name", function(){
+        let testMsg = new Message("test");
+        expect(testMsg.name).toEqual("test");
+    });
+
+    it("contains a commands array passed into the constructor as the 2nd argument", function(){
+        let testCmd1 = new Command("MOVE",'123');
+        let cmdArray=[testCmd1];
+        let testMsg = new Message("Test", cmdArray);
+        expect(testMsg.commands[0].commandType).toEqual("MOVE");
+        expect(testMsg.commands[0].value).toEqual('123');
+    });
 });
